34cf7261bd106d49096b9628bac14a306340087f
renxaio1218 <1967609349@qq.com>
2017-11-04 20:12:17 +0800

    ahange

--- src/pages/palace-museum/Palace.vue
+++ src/pages/palace-museum/Palace.vue
@@ -1,18 +1,19 @@
 <template>
 	<div>
-		<palace-header/>
 		<Comment-content/>
+		<bottom-content/>
 	</div>
 </template>
 
 <script>
-	import HeaderComponent from "./Header";
+	
 	import CommentComponent from "./Comment";
+	import bottomComponent from "./bottom";
 
 	export default {
 		components: {
-			"palace-header": HeaderComponent,
-			"Comment-content": CommentComponent
+			"Comment-content": CommentComponent,
+			"bottom-content": bottomComponent,
 	  }
    }
 </script>
