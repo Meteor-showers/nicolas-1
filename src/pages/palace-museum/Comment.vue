48e2be6352da06ebe8f0c7c11c0062e519a7288b
renxaio1218 <1967609349@qq.com>
2017-11-04 10:53:58 +0800

    chnage

---
+++ src/pages/palace-museum/Comment.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="coment-container">
+		<h2>用户评论</h2>
+		<div class="coment-container-list">
+			<div class="coment-container-item">
+				<div class="comment-title">
+					<i>BBBBB</i>
+					<span>c*2&nbsp;&nbsp;2017-09-08</span>
+					<p>直接把故宫所有需要买票的地方的票都买了，不用取票，直接刷身份证就可以进，很方便，这次去的时间可能不凑巧，最中央的几个大殿都不让进，只有钟表馆，珍宝馆还有其他的陈列文物的地方是允许进的，不过我对钟表馆里的各种钟表是欣赏不来的，珍宝馆里的玉石雕刻的确是很精巧，故宫一趟转下来腿都要断了。。真的是很大，光看没有什么感觉，只有转的时候才能真正体会到故宫的宏伟。</p>
+					<ul class="comment-imgs">
+						<li img src=""></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+ 		
+ 	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/palace-museum/go.vue
+++ src/pages/palace-museum/go.vue
@@ -1,5 +1,3 @@
 <template>
-	<div>
-		开始吧
-	</div>
+	
 </template>
