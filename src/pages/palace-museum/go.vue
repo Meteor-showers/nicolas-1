f43d105fc38fbc58fee77782a123a7ea5eadc8de
yuxi <1584629557@qq.com>
2017-11-04 11:56:12 +0800

    change

--- src/pages/palace-museum/go.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>
-		开始吧
-	</div>
-</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import List from '@/pages/list/List'
-
 Vue.use(Router)
 
 export default new Router({
