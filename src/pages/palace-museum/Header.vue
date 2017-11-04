3e9d5cbd6b5d9de164cbaa6709d93b9b34be83d2
daimengyuan <2917592984@qq.com>
2017-11-04 11:48:03 +0800

    change

---
+++ src/pages/palace-museum/Header.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="header-container">
+		<div class="header-wrap">
+			<img class="img-con" src="./img/museum1.jpg">
+		</div>
+	</div>	
+</template>
+
+
+<script>
+
+
+
+
+
+</script>
+
+
+<style>
+
+	.header-container{
+		position: relative;
+	}
+	
+
+
+
+
+</style>
---
+++ src/pages/palace-museum/Palace.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<palace-header/>
+	</div>
+</template>
+
+
+
+
+<script>
+	import HeaderComponent from "./Header"
+
+
+	export default {
+		components: {
+			"palace-header": HeaderComponent,
+	  }
+   }
+</script>
+
+
+
+
+<style>
+
+</style>
