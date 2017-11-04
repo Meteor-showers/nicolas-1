ef6ce4962e1790f2d7a9f72faa11719558246830
renxaio1218 <1967609349@qq.com>
2017-11-04 20:35:10 +0800

    sd

--- src/pages/palace-museum/Palace.vue
+++ src/pages/palace-museum/Palace.vue
@@ -1,20 +1,21 @@
 <template>
 	<div>
+		<palace-header/>
 		<Comment-content/>
 		<bottom-content/>
-		<palace-header/>
+		
 	</div>
 </template>
 
 <script>
+	import HeaderComponent from "./Header";
 	import CommentComponent from "./Comment";
 	import bottomComponent from "./bottom";
-	import HeaderComponent from "./Header";
 	export default {
 		components: {
 			"palace-header": HeaderComponent,
 			"Comment-content": CommentComponent,
-			"bottom-content": bottomComponent,
+			"bottom-content": bottomComponent
 
 	  }
    }
