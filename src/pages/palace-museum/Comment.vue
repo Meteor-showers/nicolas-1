efc1451d7ead6f3af1ffdb17e4bdf41475018f0f
Meteor-showers <13994332358@163.com>
2017-11-06 10:36:23 +0800

    fix

--- src/pages/palace-museum/Comment.vue
+++ src/pages/palace-museum/Comment.vue
@@ -8,12 +8,6 @@
 					<span>c*2&nbsp;&nbsp;2017-09-08</span>
 					<p>直接把故宫所有需要买票的地方的票都买了，不用取票，直接刷身份证就可以进，很方便，这次去的时间可能不凑巧，最中央的几个大殿都不让进，只有钟表馆，珍宝馆还有其他的陈列文物的地方是允许进的，不过我对钟表馆里的各种钟表是欣赏不来的，珍宝馆里的玉石雕刻的确是很精巧，故宫一趟转下来腿都要断了。。真的是很大，光看没有什么感觉，只有转的时候才能真正体会到故宫的宏伟。</p>
 					<ul>
-						<li><img src="./img/museum11.jpg"></li>
-						<li><img src="./img/museum1.jpg"></li>
-						<li><img src="./img/museum2.jpg"></li>
-						<li><img src="./img/museum3.jpg"></li>
-						<li><img src="./img/museum4.jpg"></li>
-						<li><img src="./img/museum5.jpg"></li>
 						<li><img src="http://img1.qunarzz.com/piao/fusion/1709/97/4b253b4f685bd02.jpg"></li>
 						<li><img src="http://img1.qunarzz.com/piao/fusion/1709/f3/d00a465647306e02.jpg"></li>
 						<li><img src="http://img1.qunarzz.com/piao/fusion/1709/4d/3ddcd92224397a02.jpg"></li>
