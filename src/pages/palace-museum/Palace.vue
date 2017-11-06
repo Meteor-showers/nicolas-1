63782854048ab93591dca9e970fa5a06925511a9
renxaio1218 <1967609349@qq.com>
2017-11-06 09:59:47 +0800

    bug

--- src/pages/palace-museum/Palace.vue
+++ src/pages/palace-museum/Palace.vue
@@ -1,12 +1,11 @@
 <template>
 	<div>
 		<palace-header/>
-<<<<<<< HEAD
 		<Comment-content/>
 		<bottom-content/>
+		
 	</div>
 </template>
-
 
 <script>
 	import HeaderComponent from "./Header";
@@ -17,26 +16,12 @@
 			"palace-header": HeaderComponent,
 			"Comment-content": CommentComponent,
 			"bottom-content": bottomComponent
-			
+
 	  }
    }
 </script>
 
-=======
-		<Recommend-where/>
-	</div>
-</template>
-<script>
-	import HeaderComponent from "./Header";
-	import RecommendComponent from "./Recommend";
-	export default {
-		components: {
-			"palace-header": HeaderComponent,
-		"Recommend-where": RecommendComponent
-	  }
-   }
-</script>
->>>>>>> 759dca03a42c941ec1cba9f27dbafec27c1ae134
 <style>
-
 </style>
+
+
