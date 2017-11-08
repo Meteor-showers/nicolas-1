491750ce5fe5536bfbe3a7f7ebcea6f5472b2df7
xiaolianlian <1530993059@qq.com>
2017-11-08 13:55:55 +0800

    'lian'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,7 +3,7 @@
  		<home-header/>
  		<swiper-content/>
  		<index-icon-swiper/>
- 		<Acitivity-view/>
+ 		<Acitivity-view :acitivityInfo="acitivityInfo"/>
  		<lazy-content/>
  		<week-content/>
  	</div>
@@ -12,12 +12,18 @@
 <script>
 import HeaderComponent from "./components/Header";
 import SwiperComponent from "./components/Swiper";
-import IconSwiper from "./components/IconSwiper"
+import IconSwiper from "./components/IconSwiper";
 import ActivityComponent from "./components/activity";
 import LazyComponent from "./components/Lazy";
 import WeekComponent from "./components/Week";
-
+import axios from 'axios';
 export default {
+	data() {
+		return {
+			acitivityInfo: []
+		}
+	},
+	
 	components: {
 		"home-header": HeaderComponent,
 		"swiper-content": SwiperComponent,
@@ -25,6 +31,26 @@ export default {
 		"Acitivity-view": ActivityComponent,
 		"lazy-content": LazyComponent,
 		"week-content": WeekComponent
+	},
+	methods: {
+		
+		getHomeData() {
+			axios.get('/static/index.json')
+				.then(this.handleGetDataSucc.bind(this))
+				.catch(this.handleGetDataErr.bind(this))
+		},
+		handleGetDataSucc(response) {
+			if (response.status === 200) {
+				this.acitivityInfo = response.data.data.acitivityInfo;
+			}
+		},
+		handleGetDataErr(err) {
+			console.log(err);
+		}
+	},
+	mounted() {
+		this.getHomeData();
+
 	}
 }
 </script>
